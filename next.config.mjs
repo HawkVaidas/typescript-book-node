bc1799ed2402c96ee0a0bd70217b90b44d0640a6
Vaidas <Vaidas@Vaidass-MacBook-Air.local>
2024-09-24 16:52:18 +0300

    changed server url

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,10 @@
 const nextConfig = {
   reactStrictMode: false,
   env: {
-    SERVER_URL: "http://localhost:3002",
-    // SERVER_URL: "",
+    // SERVER_URL: "http://localhost:3002",
+    SERVER_URL: "https://book-project-ozgc.onrender.com",
+
+    
     JWT_KEY: "book_app_jwt",
   }
 };
