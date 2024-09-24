881beac72453b7107c587120506852839bc94ea8
Vaidas <Vaidas@Vaidass-MacBook-Air.local>
2024-09-24 16:11:17 +0300

    page login header updated

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -6,7 +6,7 @@ import LoginForm from '@/components/LoginForm/LoginForm';
 const LoginPage = () => {
   return (
     <div>
-        <Header />
+        <Header  onSignOutClick={() => (false)}isUserLoggedIn={false}/>
         <h1 style={{textAlign:"center", marginTop:"5rem"}}>Login to user app</h1>
         <LoginForm/>
     </div>
